daa965ecbc6e9ef2271eed423b07cc36adb20a54
Lubos Pernis <zctqlpe@rstudio.rc.ucl.ac.uk>
2019-03-10 08:53:58 +0000

    commit

--- grf-test.R
+++ grf-test.R
@@ -1,1 +1,38 @@
 install.packages('grf')
+
+# Generate data.
+n = 2000; p = 10
+X = matrix(rnorm(n*p), n, p)
+X.test = matrix(0, 101, p)
+X.test[,1] = seq(-2, 2, length.out = 101)
+
+# Train a causal forest.
+W = rbinom(n, 1, 0.4 + 0.2 * (X[,1] > 0))
+Y = pmax(X[,1], 0) * W + X[,2] + pmin(X[,3], 0) + rnorm(n)
+tau.forest = causal_forest(X, Y, W)
+
+# Estimate treatment effects for the training data using out-of-bag prediction.
+tau.hat.oob = predict(tau.forest)
+hist(tau.hat.oob$predictions)
+
+# Estimate treatment effects for the test sample.
+tau.hat = predict(tau.forest, X.test)
+plot(X.test[,1], tau.hat$predictions, ylim = range(tau.hat$predictions, 0, 2), xlab = "x", ylab = "tau", type = "l")
+lines(X.test[,1], pmax(0, X.test[,1]), col = 2, lty = 2)
+
+# Estimate the conditional average treatment effect on the full sample (CATE).
+average_treatment_effect(tau.forest, target.sample = "all")
+
+# Estimate the conditional average treatment effect on the treated sample (CATT).
+# Here, we don't expect much difference between the CATE and the CATT, since
+# treatment assignment was randomized.
+average_treatment_effect(tau.forest, target.sample = "treated")
+
+# Add confidence intervals for heterogeneous treatment effects; growing more trees is now recommended.
+tau.forest = causal_forest(X, Y, W, num.trees = 4000)
+tau.hat = predict(tau.forest, X.test, estimate.variance = TRUE)
+sigma.hat = sqrt(tau.hat$variance.estimates)
+plot(X.test[,1], tau.hat$predictions, ylim = range(tau.hat$predictions + 1.96 * sigma.hat, tau.hat$predictions - 1.96 * sigma.hat, 0, 2), xlab = "x", ylab = "tau", type = "l")
+lines(X.test[,1], tau.hat$predictions + 1.96 * sigma.hat, col = 1, lty = 2)
+lines(X.test[,1], tau.hat$predictions - 1.96 * sigma.hat, col = 1, lty = 2)
+lines(X.test[,1], pmax(0, X.test[,1]), col = 2, lty = 1)
